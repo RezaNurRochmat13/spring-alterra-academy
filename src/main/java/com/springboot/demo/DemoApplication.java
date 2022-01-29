8e24ea89cc928c5b4401136b4324d016a3558359
Reja <rezanurrochmat@gmail.com>
2022-01-29 11:04:02 +0700

    Fix broken swagger lib

--- src/main/java/com/springboot/demo/DemoApplication.java
+++ src/main/java/com/springboot/demo/DemoApplication.java
@@ -1,5 +1,7 @@
 package com.springboot.demo;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
@@ -8,8 +10,13 @@ import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @SpringBootApplication
 public class DemoApplication {
 
+	private static Logger logger = LoggerFactory.getLogger(DemoApplication.class);
+
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
+
+		logger.info("Application is running");
+		logger.debug("Application debugging is started");
 	}
 
 }
